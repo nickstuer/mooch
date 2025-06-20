f0497a4be96610cc04ba3f357ab01433a49eaef8
Nick Stuer <nickstuer@gmail.com>
2025-06-20 16:29:53 -0500

    add file utilities

--- src/mooch/__init__.py
+++ src/mooch/__init__.py
@@ -1,9 +1,10 @@
 """mooch package initialization."""
 
 from .config.config import Config
+from .utils.file import File
 from .utils.location import Location
 from .utils.require import Require
 
-__all__ = ["Config", "Location", "Require"]
+__all__ = ["Config", "File", "Location", "Require"]
 
 __version__ = "1.0.0dev0"
