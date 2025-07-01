f3ffa3a1d395f0b13461d5c0be81da40dd124097
Nick Stuer <nickstuer@gmail.com>
2025-06-30 22:55:09 -0400

    remove config

--- src/mooch/__init__.py
+++ src/mooch/__init__.py
@@ -1,10 +1,7 @@
 """mooch package initialization."""
 
-from .config.config import Config
 from .utils.file import File
 from .utils.location import Location
 from .utils.require import Require
 
-__all__ = ["Config", "File", "Location", "Require"]
-
-__version__ = "1.0.0dev0"
+__all__ = ["File", "Location", "Require"]
