1872774ea4a93bbb16abe9726e356719fe377204
Nick Stuer <nickstuer@gmail.com>
2025-07-03 16:14:09 -0400

    fix path's module for windows

--- src/mooch/utils/paths.py
+++ src/mooch/utils/paths.py
@@ -1,4 +1,3 @@
-import os
 import sys
 from pathlib import Path
 
@@ -6,7 +5,14 @@ from pathlib import Path
 def desktop_path() -> Path:
     """Return the path to ~/Desktop."""
     if sys.platform.startswith("win"):
-        path = Path(os.environ["USERPROFILE"])
+        import ctypes.wintypes  # noqa: PLC0415
+
+        CSIDL_DESKTOPDIRECTORY = 0x10  # noqa: N806
+        SHGFP_TYPE_CURRENT = 0  # noqa: N806
+
+        buf = ctypes.create_unicode_buffer(ctypes.wintypes.MAX_PATH)
+        ctypes.windll.shell32.SHGetFolderPathW(None, CSIDL_DESKTOPDIRECTORY, None, SHGFP_TYPE_CURRENT, buf)
+        path = Path(buf.value)
     else:
         path = Path.home()
     return path / "Desktop"
