c497eb930254d3302800b3a05ecd11af3cb88159
Nick Stuer <nickstuer@gmail.com>
2025-07-03 20:16:47 -0400

    update example

--- examples/misc.py
+++ examples/misc.py
@@ -1,11 +1,16 @@
+import logging
 from typing import NoReturn
 
 from mooch.decorators import log_entry_exit, silent
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger("misc")
 
 
 @log_entry_exit
 @silent(fallback="fallback_value")
 def raise_error() -> NoReturn:
+    logger.info("function raises an error but returns 'fallback_value'")
     raise RuntimeError("fail")
 
 
