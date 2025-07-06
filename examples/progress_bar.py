895e1e2c970c80bfc418ec258da004c00b55c5d1
Nick Stuer <nickstuer@gmail.com>
2025-07-06 17:35:31 -0400

    update ProgressBar examples

--- examples/progress_bar.py
+++ examples/progress_bar.py
@@ -2,11 +2,31 @@ import time
 
 from mooch import ColoredProgressBar
 
-if __name__ == "__main__":
-    pb = ColoredProgressBar(
-        total=5,
-    )
+# Basic Example
+pb = ColoredProgressBar(100)
+for _ in range(100):
+    time.sleep(0.1)
+    pb.update()
 
-    for _ in range(88):
-        time.sleep(0.5)
-        pb.update()
+# Finish Early
+pb = ColoredProgressBar(100)
+for i in range(100):
+    time.sleep(0.1)
+    pb.update()
+    if i == 50:
+        pb.finish()
+        break
+
+# Delayed Start
+pb = ColoredProgressBar(100, auto_start=False)
+time.sleep(1)  # Simulate some work before starting
+pb.start()
+for _ in range(100):
+    time.sleep(0.1)
+    pb.update()
+
+# Overrun Example
+pb = ColoredProgressBar(5)
+for _ in range(5):  # Intentionally overrun
+    time.sleep(0.1)
+pb.update()  # This will trigger the overrun warning
